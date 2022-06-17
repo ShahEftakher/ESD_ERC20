5f7bd2d29f640af092e08c7fffc66a50c0afd097
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-06-17 17:44:43 +0600

    Added external methods

--- contracts/ESDToken.sol
+++ contracts/ESDToken.sol
@@ -13,7 +13,7 @@ contract ESDToken is ERC20 {
     ) ERC20("ESD Token", "ESD") {
         admin = _admin;
         maxCirculation = _maxCirculation;
-        _mint(msg.sender, _initialSupply * (10**18));
+        _mint(msg.sender, toESDwei(_initialSupply));
     }
 
     modifier onlyOwner() {
@@ -21,22 +21,37 @@ contract ESDToken is ERC20 {
         _;
     }
 
-    function mintESD(uint256 _amount) external onlyOwner {
-        // require(totalSupply() + _amount <= maxCirculation, "Exceeding limit");
-        _mint(msg.sender, _amount * (10**18));
+    function toESDwei(uint256 _ESD) internal view returns (uint256) {
+        return _ESD * 10**decimals();
     }
 
+    function mintESD(uint256 _amount) external onlyOwner {
+        // require(totalSupply() + _amount <= maxCirculation, "Exceeding limit");
+        _mint(msg.sender, toESDwei(_amount));
+    }
+
+    //external tranfer fn
+    function transferTo(address _to, uint256 _amount) external {
+        transfer(_to, toESDwei(_amount));
+    }
+
+    //external _transerFrom
     function _transferFrom(
         address _from,
         address _to,
         uint256 _amount
     ) external {
-        transferFrom(_from, _to, _amount * 10**18);
+        transferFrom(_from, _to, toESDwei(_amount));
     }
 
+    //external approve
     function approveTx(address _spender, uint256 _amount) external {
-        approve(_spender, _amount * 10**18);
+        approve(_spender, toESDwei(_amount));
     }
 
-    
+
+    //override decimal
+    function decimals() public view virtual override returns (uint8) {
+        return 10;
+    }
 }
--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,5 +1,5 @@
 const ESDToken = artifacts.require('ESDToken.sol');
 
 module.exports = function (deployer, _network, accounts) {
-  deployer.deploy(ESDToken, 10000, accounts[0], 1000000);
+  deployer.deploy(ESDToken, 20000, accounts[0], 1000000);
 };
