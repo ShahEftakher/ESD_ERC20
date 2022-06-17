33590f09584732b8e3c68c288819a926503b0bea
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-06-17 11:21:24 +0600

    Added external functions for smart contract tracsaction

--- contracts/ESDToken.sol
+++ contracts/ESDToken.sol
@@ -4,16 +4,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ESDToken is ERC20 {
     address private admin;
-    uint256 totalCirculation;
+    uint256 maxCirculation;
 
     constructor(
         uint256 _initialSupply,
         address _admin,
-        uint256 _totalCirculation
+        uint256 _maxCirculation
     ) ERC20("ESD Token", "ESD") {
         admin = _admin;
-        totalCirculation = _totalCirculation;
-        _mint(msg.sender, _initialSupply);
+        maxCirculation = _maxCirculation;
+        _mint(msg.sender, _initialSupply * (10**18));
     }
 
     modifier onlyOwner() {
@@ -22,7 +22,21 @@ contract ESDToken is ERC20 {
     }
 
     function mintESD(uint256 _amount) external onlyOwner {
-        // require(totalSupply() + _amount <= totalCirculation, "Exceeding limit");
-        _mint(msg.sender, _amount);
+        // require(totalSupply() + _amount <= maxCirculation, "Exceeding limit");
+        _mint(msg.sender, _amount * (10**18));
     }
+
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external {
+        transferFrom(_from, _to, _amount * 10**18);
+    }
+
+    function approveTx(address _spender, uint256 _amount) external {
+        approve(_spender, _amount * 10**18);
+    }
+
+    
 }
